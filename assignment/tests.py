b19c71d33e2646475f072d4e16d32a64d7d83d81
nirmalasainsara <nirmlasainsara909@gmail.com>
2021-06-19 21:20:43 +0530

    add test

--- assignment/tests.py
+++ assignment/tests.py
@@ -24,11 +24,17 @@ class RegistrationTestcase(APITestCase):
 
 class CourseTestcase(APITestCase):
     def test_course(self):
+        headers = {
+            "Authorization": "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjI0MTE3NzYxLCJqdGkiOiJiMWMwOWZjOGU1MzE0MjIyOGIxOWE2YWM0MGYwODU5YSIsInVzZXJfaWQiOjN9.FYXkNvoVBMAjrrkc7w-JyPYN_3OlRJ2_FrmCT3z_rNA",
+            "Cookie": "csrftoken=CBIdCKHZmkw2p23Csg4apfg5mcl9YD48gmgpltf86rfOaZFXOKGtJoDnKBEUBoPs",
+        }
         data = {
             "name": "Book",
             "author": "chetan bagat",
             "date": datetime.datetime(2021, 6, 16, 10, 24, 0, 0, pytz.UTC),
             "price": 90,
         }
-        response = self.client.post("http://127.0.0.1:8000/habbit/add_course/", data)
+        response = self.client.post(
+            "http://127.0.0.1:8000/habbit/add_course/", data=data, **headers
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
